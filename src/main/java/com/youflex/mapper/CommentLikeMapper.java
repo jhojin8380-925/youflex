adcdd0e57cb6fea57d7382ae32aad6f7eaabe3bf
ungjo <ungjo911@gmail.com>
2026-07-14 14:05:01 +0900

    리뷰 관련 인터페이스에 @Mapper 어노테이션 추가 (#13)
    * java Review 파일 생성
    
    * Review관련 파일 생성
    
    * ReviewMapper.xml 타입 정의문 추가
    
    * 수정중
    
    * 수정중
    
    * (리뷰, 댓글)interface, xml 추가
    
    * ReviewService.java 작성중
    
    * 리뷰관련 인터페이스에 @Mapper 추가

--- src/main/java/com/youflex/mapper/CommentLikeMapper.java
+++ src/main/java/com/youflex/mapper/CommentLikeMapper.java
@@ -1,5 +1,8 @@
 package com.youflex.mapper;
 
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
 public interface CommentLikeMapper {
 
 }
--- src/main/java/com/youflex/service/ReviewService.java
+++ src/main/java/com/youflex/service/ReviewService.java
@@ -1,10 +1,14 @@
 package com.youflex.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
+import com.youflex.dto.ReviewDTO;
 import com.youflex.mapper.BookmarkMapper;
 import com.youflex.mapper.CommentMapper;
-import com.youflex.mapper.CommentLikeMapper;
+import com.youflex.mapper.ReviewDraftMapper;
+import com.youflex.mapper.ReviewLikeMapper;
 import com.youflex.mapper.ReviewMapper;
 import com.youflex.mapper.ReviewReportMapper;
 
@@ -15,7 +19,34 @@ import lombok.RequiredArgsConstructor;
 public class ReviewService {
 	private final ReviewMapper reviewMapper;
 	private final CommentMapper commentMapper;
-	private final CommentLikeMapper likeMapper;
+	// 좋아요, 북마크, 신고, 게시글 입시저장
+	private final ReviewLikeMapper reviewLikeMapper;
 	private final BookmarkMapper bookmarkMapper;
 	private final ReviewReportMapper reviewReportMapper;
+	private final ReviewDraftMapper reviewDraftMapper;
+	
+//	1) 게시글 저장
+	public void write(ReviewDTO reviewDTO) {
+		reviewMapper.write(reviewDTO);
+	}
+	
+//	2) 전체 게시글 목록 조회
+	public List<ReviewDTO> findAll(String keywor, int offset, int size){
+		return reviewMapper.findAll(keywor, offset, size);
+	}
+	
+//	+) 검색 결과 전체 개수 조회
+	public int countAll(String keyword) {
+		return reviewMapper.countAll(keyword);
+	}
+	
+//	3) 게시글 상세 조회
+	public ReviewDTO findById(int postId) {
+		return reviewMapper.findById(postId);
+	}
+	
+//	4) 게시글 수정
+	public void update(ReviewDTO reviewDTO) {
+		reviewMapper.update(reviewDTO);
+	}
 }
